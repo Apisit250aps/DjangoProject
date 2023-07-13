d32079a4be8827145396013654456ea0c60b4d48
Apisit250aps <aps.apisit250@gmail.com>
2023-07-13 11:50:03 +0700

    add admin template

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -118,8 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [Path.joinpath(BASE_DIR, 'statics')]
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [Path.joinpath(BASE_DIR, 'static')]
 STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
