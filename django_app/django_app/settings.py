d17947245a7d59aa546310f7bceccc006018a165
Apisit250aps <aps.apisit250@gmail.com>
2023-07-13 11:20:19 +0700

    add static - template dir

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -117,7 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [Path.joinpath(BASE_DIR, 'statics')]
+STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
